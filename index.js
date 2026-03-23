f1f4d62f97a1aa00ed30579a68d78d2f1382a581
yuringeno <nicolecheptoo152@gmail.com>
2026-03-23 11:11:45 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 
-
-
-
-
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+
+
+
+
+
+
+
+
+
+
+
+
