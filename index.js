ef779821c2efdb229b6bc132954db27cf2024e60
yuringeno <nicolecheptoo152@gmail.com>
2026-03-23 11:15:47 +0300

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,26 @@
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
 
+calculateTax = (price, taxRate) => {
+    return price * (taxRate / 100);
+}   
+
+convertToUpperCase = (str) => {
+    return str.toUpperCase();
+}   
+
+findMaximum = (arr) => {
+    return Math.max(...arr);
+}   
+isPalindrome = (str) => {
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}   
+calculateDiscountedPrice = (price, discount) => {
+    return price - (price * (discount / 100));
+}   
+
+
 
 
 
